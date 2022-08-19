486e6abb06a936feaaf0eb43040ab16589e2f950
Mystery <mysteryblso@gmail.com>
2022-08-19 10:17:23 +0300

    Add a function that makes a random choice

---
+++ script.js
@@ -0,0 +1,10 @@
+function getComputerChoice(){
+    let choice
+    choice = Math.floor(Math.random() * 3) + 1
+    if(choice == 1)
+        return "Rock"
+    else if(choice == 2)
+        return "Paper"
+    else if(choice == 3)
+        return "Scissors"
+}
